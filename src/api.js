9665b6758d93ee54e45244685f4c43d6e911296d
sohamx5800 <sohamadhikary707@gmail.com>
2025-04-05 03:05:38 +0530

    Added extra link to frontend UI

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+console.log('Using BACKEND_URL:', BACKEND_URL);
 export const apiFetch = async (endpoint, options = {}) => {
+  console.log('Fetching:', `${BACKEND_URL}${endpoint}`);
   const response = await fetch(`${BACKEND_URL}${endpoint}`, {
     ...options,
     headers: { "Content-Type": "application/json", ...options.headers },
